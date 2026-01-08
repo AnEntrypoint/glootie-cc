7fd732430f4208b42e0724fafccda5e40a7325e2
lanmower <almagestfraternite@gmail.com>
2026-01-08 11:48:17 +0200

    Update prompt-submit-hook.js

--- prompt-submit-hook.js
+++ prompt-submit-hook.js
@@ -32,7 +32,7 @@ try {
     }
   }
 
-  const additionalContext = outputs.join('\n\n');
+  const additionalContext = outputs.join('Use APEX to coordinate all plans and the glootie subagent to execute all plans\n\n');
 
   const result = {
     hookSpecificOutput: {
